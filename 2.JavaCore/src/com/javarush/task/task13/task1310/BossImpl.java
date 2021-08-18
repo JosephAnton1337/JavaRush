30f8fe1662bfa086cada4d28b1be7c849ee1f7bd
Pavel Rudnev <rudnevp3@gmail.com>
2021-08-18 18:01:20 +0300

    Level13

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1310/BossImpl.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task13.task1310;
+
+public class BossImpl implements Solution.Boss {
+    @Override
+    public void use(Solution.Person person) {
+
+    }
+
+    @Override
+    public void startToWork() {
+
+    }
+
+    @Override
+    public boolean inspiresOthersToWork() {
+        return true;
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task13/task1310/SecretaryImpl.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task13.task1310;
+
+public class SecretaryImpl implements Solution.Secretary {
+    @Override
+    public void use(Solution.Person person) {
+
+    }
+
+    @Override
+    public void startToWork() {
+
+    }
+}
